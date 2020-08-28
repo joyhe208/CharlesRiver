868686a3d3820b1b153d5e5d89c5ac1f3936babc
Joy He <joyhe208@macbook-pro.myfiosgateway.com>
2020-08-27 20:20:30 -0400

    added markers

--- charlesmap.py
+++ charlesmap.py
@@ -3,15 +3,37 @@ import pandas as pd
 from pandas import read_html
 
 #Extract Table from Wikipedia Page
-bridgelist = pd.read_html('CharlesRiverCrossings.html', header = 0, index_col = None, attrs = {'class':'wikitable'})[0]
+bridgelistdf = pd.read_html('CharlesRiverCrossings.html', header = 0, index_col = None, attrs = {'class':'wikitable'})[0]
 
-coordinates = bridgelist.loc[:, 'Coordinates']
-#print(coordinates.head(15))
+#get rid of the NAN rows for coordinates
+bridgelistdf.dropna(subset = ['Coordinates'], inplace = True)
+
+#isolate the coordinates
+coordinatesdf = bridgelistdf.loc[:, 'Coordinates']
+
+lats = []
+longs = []
 
 #get list of coordinates as numbers
-latitude = []
-longitude = []
+for coordinate in coordinatesdf:
+	
+	if("°N" in coordinate.split(" / ")[1].split(" ")[0]):
+		lats.append(float(coordinate.split(" / ")[1].split(" ")[0].split("°N")[0].replace('\ufeff','')))
+
+	if("°S" in coordinate.split(" / ")[1].split(" ")[0]):
+		lats.append(-float(coordinate.split(" / ")[1].split(" ")[0].split("°S")[0].replace('\ufeff','')))
+
+	if("°W" in coordinate.split(" / ")[1].split(" ")[1]):
+		longs.append(-float(coordinate.split(" / ")[1].split(" ")[1].split("°W")[0].replace('\ufeff','')))
+
+	if("°E" in coordinate.split(" / ")[1].split(" ")[1]):
+		longs.append(float(coordinate.split(" / ")[1].split(" ")[1].split("°E")[0].replace('\ufeff','')))
 
 #Create Map
-m = folium.Map(io = [42.3736, -71.1097], zoom_start = 12)
+m = folium.Map(location = [42.3736, -71.1097], zoom_start = 12)
+
+#create markers
+for i in range(len(lats)):
+	folium.Marker([lats[i], longs[i]]).add_to(m)
+
 m.save('charlesmap.html')
