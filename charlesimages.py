cc7fd78f5f9e436de79cb08b01e544c8a1dbde12
Joy He <joyhe208@macbook-pro.myfiosgateway.com>
2020-08-30 17:36:39 -0400

    added images for bridges I've run across cause downloading 140 images is a no from me dawg but holy crap this took soooo long to debug but I'm so happy now even though it doesn't look that great:)

--- charlesimages.py
+++ charlesimages.py
@@ -3,5 +3,23 @@ from charlesmap import *
 
 html_doc = open('CharlesRiverCrossings.html', 'r')
 soup = BeautifulSoup(html_doc, 'html.parser')
-if __name__ == '__main__':
-	print(soup.prettify())
+table = soup.table	
+table_rows = table.find_all('tr')
+table_rows.remove(table_rows[4])
+table_rows.remove(table_rows[0])
+	# for i in range(len(lats)):
+	# 	print(i)
+	# 	dividers = table_rows[i].find_all('td')
+	# 	if len(dividers[len(dividers)-1].find_all('img', limit = 1))!=0:
+	# 		image = str(dividers[len(dividers)-1].find('img').get('src'))
+	# 		image = image.replace("/thumb", "")
+	# 		try:
+	# 			image = image.split(".jpg")[0]
+	# 			image = "https:" + image + ".jpg"
+	# 		except:
+	# 			image = image.split("/400px")[0]
+	# 			image = "https:" + image 
+
+	# 		dl_img(image, "pic" + str(i))
+
+	
--- charlesmap.py
+++ charlesmap.py
@@ -1,12 +1,16 @@
 import folium
+from folium import IFrame
 import pandas as pd
 from pandas import read_html
 from flask import Flask
 import numpy as np
 from bs4 import BeautifulSoup
+import urllib.request
+from charlesimages import *
 
 #Extract Table from Wikipedia Page
 bridgelistdf = pd.read_html('CharlesRiverCrossings.html', header = 0, index_col = None, attrs = {'class':'wikitable'})[0]
+
 
 #get rid of the NAN rows for coordinates
 bridgelistdf.dropna(subset = ['Coordinates'], inplace = True)
@@ -20,7 +24,7 @@ for bridgename in bridgenamesdf:
 
 
 #bridges I've run across
-bridgesrun = ['Charlestown Bridge', 'MBTA Green Line Lechmere Viaduct', 'Longfellow Bridge', 'Harvard Bridge', 'Boston University Bridge', 'River Street Bridge', 'Western Avenue Bridge', 'John W. Weeks Bridge', 'Anderson Memorial Bridge', 'Eliot Bridge', 'Arsenal Street Bridge', 'North Beacon Street Bridge', 'Watertown Bridge', 'Cpl. Joseph U. Thompson Footbridge (new)', 'Bridge Street Bridge', 'Blue Heron Footbridge', 'Farwell Street Bridge', 'Mary T. Early Footbridge', 'Charles F. Graceffa Bridge', 'Elm Street Bridge', 'Richard Landry Park footbridge', 'Moody Street bridge', 'Gold Star Mothers Bridge', 'Footbridge from Riverside Rd., Newton to Recreation Rd., Weston', 'Riverside Park Footbridge']
+bridgesrun = ['Charlestown Bridge', 'MBTA Green Line Lechmere Viaduct', 'Longfellow Bridge', 'Harvard Bridge', 'Boston University Bridge', 'River Street Bridge', 'Western Avenue Bridge', 'John W. Weeks Bridge', 'Anderson Memorial Bridge', 'Eliot Bridge', 'Arsenal Street Bridge', 'North Beacon Street Bridge', 'Watertown Bridge', 'Cpl. Joseph U. Thompson Footbridge (new)', 'Bridge Street Bridge1', 'Blue Heron Footbridge', 'Farwell Street Bridge', 'Mary T. Early Footbridge', 'Charles F. Graceffa Bridge', 'Elm Street Bridge', 'Richard Landry Park footbridge', 'Moody Street bridge', 'Gold Star Mothers Bridge', 'Road bridge from Riverside Rd., Newton to Recreation Rd., Weston', 'Riverside Park Footbridge']
 
 #isolate the coordinates
 coordinatesdf = bridgelistdf.loc[:, 'Coordinates']
@@ -43,15 +47,53 @@ for coordinate in coordinatesdf:
 	if("°E" in coordinate.split(" / ")[1].split(" ")[1]):
 		longs.append(float(coordinate.split(" / ")[1].split(" ")[1].split("°E")[0].replace('\ufeff','')))
 
+def dl_img(url, filename):
+	path = "bridgeimages/" + filename + ".jpg"
+	urllib.request.urlretrieve(url, path)
+
+#prepare the html wiki page to get the images
+#table_rows now has all the html stuff for each row as a list
+html_doc = open('CharlesRiverCrossings.html', 'r')
+soup = BeautifulSoup(html_doc, 'html.parser')
+table = soup.table	
+table_rows = table.find_all('tr')
+table_rows.remove(table_rows[4])
+table_rows.remove(table_rows[0])
+
 if __name__ == "__main__":
 	#Create Map
 	m = folium.Map(location = [42.3736, -71.1097], zoom_start = 12)
 
 	#create markers
 	for i in range(len(lats)):
-		pop = '<strong style="color:#967bb6;">' + bridgenames[i] + '</strong>'
+		#image = '<img src =400px-Charlestown_Bridge_MA_02.jpg>'
+
+		# if(len(table_rows[i].find_all('img', limit =1))!=0):
+		# 	image = str(table_rows[i].find_all('img', limit =1)[0])
+		# image = ""
+		# dividers = table_rows[0].find_all('td')
+		# if(len(dividers[len(dividers)-1].find_all('img', limit = 1)) !=0):
+		# 	image = str(dividers[len(dividers)-1].find_all('img', limit = 1)[0].get('src'))
+		# 	image = image.replace("/thumb", "")
+		# 	image = image.split("/200px")[0]
+		# 	image = "https:" + image
+		# image = "400px-" + image
+		# image = image.replace(" ", "_")
+		# image = "https://lh3.googleusercontent.com/proxy/QgxA7piVCDYQGdQtRhHPirYXtYMhE0nKZ9rgorwp_vbrNjOD0VxANOrJnQ-J8FrcirZeExC-YCETpxE4JtCMeCVyzEBRFmf3UqmI9hf1Eq2a0RLkuFcPnSU"
+		# image = "<img src =" + image + ">"
+		
+		pop = '<strong style="color:#967bb6;width=500%;height=500%">' + bridgenames[i] + '</strong><br>'
+
 		if(bridgenames[i] in bridgesrun):
 			col = 'green'
+			# dividers = table_rows[i].find_all('td')
+			# if(len(dividers[len(dividers)-1].find_all('img', limit = 1)) !=0):
+			# 	image = str(dividers[len(dividers)-1].find_all('img', limit = 1)[0].get('src'))
+			# 	image = image.replace("/thumb", "")
+			# 	image = image.split("/200px")[0]
+			# 	image = "https:" + image
+			# 	dl_img(image, "pic" + str(i))
+			pop = pop + '<img src= "pic' + str(i) + '.jpg" style = "width:500%;height:500%;"/>'
 		else:
 			col = 'red'
 		folium.Marker([lats[i], longs[i]], popup = pop, icon = folium.Icon(color = col)).add_to(m)
